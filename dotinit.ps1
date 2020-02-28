e958020ee06eae7323bf075d641293468434bf37
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-02-29 00:33:12 +0100

    first version of script that initialize a dotnet project/solution

---
+++ dotinit.ps1
@@ -0,0 +1,148 @@
+param(
+    [Parameter(Mandatory = $true)][string]$project,
+    [switch]$dryrun = $false,
+    [string]$repoarea = "D:\dev\rep",
+    [switch]$nogit = $false,
+    [string]$type = "classlib",
+    [switch]$nosln = $false
+)
+
+#
+# Log fx
+#
+function WriteAction {
+    param (
+        [string]$msg
+    )
+    
+    Write-Host $msg -BackgroundColor Gray -ForegroundColor Blue
+}
+
+function WriteDryRun {
+    param (
+        [string]$msg
+    )
+    
+    Write-Host ("   dryrun: " + $msg) -BackgroundColor Gray -ForegroundColor Black
+}
+#
+
+[string]$RootDir = ""
+[string]$SolutionDir = ""
+[string]$ProjectDir = ""
+if ($nosln) {
+    $ProjectDir = Join-Path -Path $repoarea -ChildPath $project
+    $RootDir = $ProjectDir
+}
+else {
+    $SolutionDir = Join-Path -Path $repoarea -ChildPath $project
+    $ProjectDir = Join-Path -Path $SolutionDir -ChildPath $project
+    $RootDir = $SolutionDir
+}
+
+#
+# Test if can make project
+#
+
+if (-not (Test-Path -Path $repoarea)) {
+    Write-Error ("Repository area directory: " + $repoarea + " does not exists : terminates");
+    exit 1
+}
+
+if ($nosln) {
+    if (Test-Path -Path $ProjectDir) {
+        Write-Error ("Project directory: " + $ProjectDir + " exists : terminates")
+        exit 2
+    }
+}
+else {
+    if (Test-Path -Path $SolutionDir) {
+        Write-Error ("Solution directory: " + $SolutionDir + " exists : terminates")
+        exit 2
+    }
+}
+
+#
+# Start doing
+#
+
+if (-not $nosln) {
+    Write-Host ""
+    WriteAction ("Creates solution directory: " + $SolutionDir)
+    if ($dryrun) {
+        WriteDryRun ("solution directory not created")
+    }
+    else {
+        New-Item -Path $SolutionDir -ItemType "directory"
+    }
+}
+
+Write-Host ""
+WriteAction ("Creates project directory: " + $ProjectDir)
+if ($dryrun) {
+    WriteDryRun ("dryrun: project directory not created")
+}
+else {
+    New-Item -Path $ProjectDir -ItemType "directory"
+}
+
+if (-not $dryrun) {
+    Push-Location $ProjectDir
+}
+
+Write-Host ""
+WriteAction ("Creates dotnet project of type '" + $type + "' in " + $ProjectDir)
+if (-not $dryrun) {
+    dotnet.exe new $type
+}
+else {
+    WriteDryRun("project not created")
+}
+
+if (-not $dryrun) {
+    Pop-Location
+    Push-Location $RootDir
+}
+
+if (-not $nosln) {
+    [string]$ProjectFile = Join-Path -Path $ProjectDir -ChildPath ($project + ".csproj")
+    Write-Host ""
+    WriteAction ("Creates solution file in " + $RootDir + " and adds project file " + $ProjectFile)
+    if (-not $dryrun) {
+        dotnet.exe new sln
+        dotnet.exe sln add $ProjectFile
+    }
+    else {
+        WriteDryRun("solution not created")
+    }
+}
+
+if (-not $nogit) {
+    Write-Host ""
+    WriteAction ("Creates git repository, gitignore file and initial commit in " + $RootDir)
+    if (-not $dryrun) {
+        git.exe init
+        dotnet.exe new gitignore
+        git.exe add .
+        git.exe commit -m 'initial commit'
+    }
+    else {
+        WriteDryRun ("git repository not created")
+    }
+}
+
+#
+# Done
+#
+
+if (-not $dryrun) {
+    Pop-Location
+}
+
+Write-Host ""
+WriteAction "Project created OK"
+if ($dryrun) {
+    WriteDryRun ("not actually created")
+}
+
+exit 0
