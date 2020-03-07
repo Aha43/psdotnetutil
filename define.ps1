8a00c6f933387b308eca5c96b526f804b3d746b3
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-03-08 00:00:49 +0100

    Renamed Define.ps1 to all lower case and added inline comment of purpose

--- define.ps1
+++ define.ps1
@@ -1,3 +1,4 @@
+# Run this file to get functions defined. Future version may make this into a module removing the need for this file.
 . (Join-Path -Path $PSScriptRoot -ChildPath ".\writehostf.ps1")
 . (Join-Path -Path $PSScriptRoot -ChildPath ".\dotinitf.ps1")
 . (Join-Path -Path $PSScriptRoot -ChildPath ".\dotaddf.ps1")
